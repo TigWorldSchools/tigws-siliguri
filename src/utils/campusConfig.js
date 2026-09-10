213476214f7eadfd6b6b9b563de767adcf8b0ee5
Kaushik <kaushikm@technoindiagroup.in>
2026-09-10 13:36:52 +0530

    Changes in Mandatory Disclosure of Siliguri

--- src/utils/campusConfig.js
+++ src/utils/campusConfig.js
@@ -215,12 +215,12 @@ export const campusConfig = {
         {
           id: 5,
           info: "PRINCIPAL NAME & QUALIFICATION",
-          details: "Mrs. Malancha Ghosh",
+          details: "Mrs.Nivedita Chakraborty",
         },
         {
           id: 6,
           info: "PRINCIPAL QUALIFICATION",
-          details: "M.A. in English, B.Ed.",
+          details: "M.A. in Political Science, B.Ed.",
         },
         { id: 7, info: "SCHOOL EMAIL ID", details: "admin.slg@tigws.in" },
         {
@@ -233,7 +233,7 @@ export const campusConfig = {
         {
           id: 1,
           info: "COPIES OF AFFILIATION/UPGRADATION LETTER AND RECENT EXTENSION OF AFFILIATION, IF ANY",
-          link: "#",
+          link: "/pdf/siliguri-mpd/affiliation_class_X.pdf",
         },
         {
           id: 2,
@@ -268,60 +268,60 @@ export const campusConfig = {
         {
           id: 8,
           info: "COPIES OF VALID WATER, HEALTH AND SANITATION CERTIFICATES",
-          link: "/pdf/siliguri-mpd/WATER,_HEALTH_AND_SANITATION.pdf",
+          link: "/pdf/siliguri-mpd/water_health_sanitation.pdf",
         },
       ],
       academics: [
         {
           id: 1,
           info: "FEE STRUCTURE OF THE SCHOOL",
-          link: "/pdf/siliguri-mpd/FEE_STRUCTURE.pdf",
+          link: "/pdf/siliguri-mpd/fee_structure _2026-27.pdf",
         },
-        { id: 2, info: "ANNUAL ACADEMIC CALENDAR", link: "#" },
-        { id: 3, info: "LIST OF SCHOOL MANAGEMENT COMMITTEE (SMC)", link: "#" },
+        { id: 2, info: "ANNUAL ACADEMIC CALENDAR", link: "/pdf/siliguri-mpd/annual_calendar_2026-27.pdf" },
+        { id: 3, info: "LIST OF SCHOOL MANAGEMENT COMMITTEE (SMC)", link: "/pdf/siliguri-mpd/school_management_committee.pdf" },
         {
           id: 4,
           info: "LIST OF PARENTS TEACHERS ASSOCIATION (PTA) MEMBERS",
-          link: "#",
+          link: "/pdf/siliguri-mpd/parent_teacher_association.pdf",
         },
         {
           id: 5,
           info: "ANNUAL REPORT",
-          link: "/pdf/siliguri-mpd/ANNUAL_REPORT_siliguri.pdf",
+          link: "/pdf/siliguri-mpd/annual_report_2025-26.pdf",
         },
         {
           id: 6,
           info: "DETAILS OF CURRICULUM",
-          link: "https://cbseacademic.nic.in/curriculum_2025.html",
+          link: "https://cbseacademic.nic.in/curriculum_2027.html",
         },
         {
           id: 7,
           info: "SAMPLE OF TRANSFER CERTIFICATE",
-          link: "/pdf/siliguri-mpd/SAMPLE_OF_TC_siliguri.pdf",
+          link: "/pdf/siliguri-mpd/transfer_certificate_sample.pdf",
         },
         {
           id: 8,
           info: "DETAILS OF TEACHERS TRAINING",
-          link: "/pdf/siliguri-mpd/Details_of_Teachers_Training.pdf",
+          link: "/pdf/siliguri-mpd/teachers'_training_2025-26.pdf",
         },
         {
           id: 9,
           info: "STAFF STATEMENT 2024-25",
-          link: "/pdf/siliguri-mpd/STAFF_STATEMENT_2024-25_TIGWS.pdf",
+          link: "/pdf/siliguri-mpd/self_affidavit.pdf",
         },
-        {
-          id: 10,
-          info: "SELF AFFIDAVIT",
-          link: "/pdf/siliguri-mpd/SELF-AFFIDAVIT.pdf",
-        },
+        // {
+        //   id: 10,
+        //   info: "SELF AFFIDAVIT",
+        //   link: "/pdf/siliguri-mpd/SELF-AFFIDAVIT.pdf",
+        // },
       ],
       staffInfo: [
         { id: 1, info: "PRINCIPAL", details: "01" },
-        { id: 2, info: "TOTAL NO. OF TEACHERS", details: "23" },
-        { id: 3, info: "PGT", details: "0" },
-        { id: 4, info: "TGT", details: "10" },
-        { id: 5, info: "PRT", details: "13" },
-        { id: 6, info: "TEACHERS SECTION RATIO", details: "1:5" },
+        { id: 2, info: "TOTAL NO. OF TEACHERS", details: "25" },
+        { id: 3, info: "PGT", details: "11" },
+        { id: 4, info: "TGT", details: "6" },
+        { id: 5, info: "PRT", details: "9" },
+        { id: 6, info: "TEACHERS SECTION RATIO", details: "1.5:1" },
         { id: 7, info: "DETAILS OF SPECIAL EDUCATOR", details: "01" },
         {
           id: 8,
@@ -333,12 +333,12 @@ export const campusConfig = {
         {
           id: 1,
           info: "TOTAL CAMPUS AREA OF THE SCHOOL (IN SQ. MTR.)",
-          details: "12,140.569",
+          details: "12,140.57",
         },
         {
           id: 2,
           info: "NO. AND SIZE OF THE CLASS ROOMS (IN SQ. MTR.)",
-          details: "15 classrooms (46.50 sq.m each)",
+          details: "52 Class Rooms(46.50 sq.mt each)",
         },
         {
           id: 3,
@@ -346,8 +346,8 @@ export const campusConfig = {
           details: "6 Labs (Average 75 sq.m each)",
         },
         { id: 4, info: "INTERNET FACILITY", details: "Yes" },
-        { id: 5, info: "NO. OF GIRLS TOILETS", details: "30" },
-        { id: 6, info: "NO. OF BOYS TOILETS", details: "NA" },
+        { id: 5, info: "NO. OF GIRLS TOILETS", details: "46" },
+        { id: 6, info: "NO. OF BOYS TOILETS", details: "2" },
         {
           id: 7,
           info: "LINK OF YOUTUBE VIDEO OF THE INSPECTION OF SCHOOL COVERING THE INFRASTRUCTURE OF THE SCHOOL",
